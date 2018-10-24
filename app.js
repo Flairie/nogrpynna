08e3a23f3ddcac026de10f42e699fa7f2036bc0c
Flairie <37420327+Flairie@users.noreply.github.com>
2018-10-24 13:11:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 // TypeScript
-import { Bot } from 'node-vk-bot'
+const VkBot = require('node-vk-bot')
  
-// ES5
-const { Bot } = require('node-vk-bot')
  
-const bot = new Bot({
-  token: process.env.BOT_TOKEN,
-  group_id: 172340531
-}).start()
+const bot = new VkBot(process.env.BOT_TOKEN,)
  
-bot.get(/Hi|Hello|Hey/i, (message, exec, reply) => {
-  const options =  { forward_messages: message.id }
- 
-  reply('Hello!', options)
+bot.command('/start', (ctx) => {
+  ctx.reply('Hello!')
 })
+
+bot.startPolling()
