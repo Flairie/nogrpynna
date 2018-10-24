4d304b4f07bd9fb3eaed3a40682ee2691757d1d4
Flairie <37420327+Flairie@users.noreply.github.com>
2018-10-24 13:19:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const VkBot = require('node-vk-bot')
  
  
-const bot = new VkBot(process.env.BOT_TOKEN,)
+const bot = new VkBot(process.env.BOT_TOKEN)
  
 bot.command('/start', (ctx) => {
   ctx.reply('Hello!')
